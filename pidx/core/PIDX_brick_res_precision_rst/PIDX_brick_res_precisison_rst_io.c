e703c7584fb019d68ae9e990e49abade9dd58eb4
kokofan <kokofan@kokos-MacBook-Pro.local>
2020-05-25 22:33:02 -0500

    add wavelet transform

--- pidx/core/PIDX_brick_res_precision_rst/PIDX_brick_res_precisison_rst_io.c
+++ pidx/core/PIDX_brick_res_precision_rst/PIDX_brick_res_precisison_rst_io.c
@@ -268,6 +268,176 @@ PIDX_return_code PIDX_brick_res_precision_rst_buf_read_and_aggregate(PIDX_brick_
 }
 
 
+// A wavelet helper
+void wavelet_helper(unsigned char* buf, int step, int ng_step, int flag, int bits, uint64_t x, uint64_t y, uint64_t z, char* type_name)
+{
+    int si = ng_step; int sj = ng_step; int sk = ng_step;
+
+    // Define the start position based on orientations (x, y, z)
+    if (flag == 0)
+        sj = step;
+    if (flag == 1)
+        si = step;
+    if (flag == 2)
+        sk = step;
+
+    int neighbor_ind = 1;
+
+	// data types
+    unsigned char c_data = 0;
+    unsigned char c_neigb = 0;
+    short s_data = 0;
+    short s_neigb = 0;
+    float f_data = 0;
+    float f_neigb = 0;
+    double d_data = 0;
+    double d_neigb = 0;
+    int i_data = 0;
+    int i_neigb = 0;
+    uint64_t u64i_data = 0;
+    uint64_t u64i_neigb = 0;
+    int64_t i64_data = 0;
+    int64_t i64_neigb = 0;
+
+
+    for (int k = 0; k < z; k+=sk)
+    {
+        for (int i = 0; i < y; i+=si)
+        {
+            for (int j = 0; j < x; j+=sj)
+            {
+                int index = k * x * y + i * x + j;
+                // Define the neighbor position based on orientations (x, y, z)
+                if (flag == 0)
+                	neighbor_ind = index + ng_step;
+                if (flag == 1)
+                    neighbor_ind = index + ng_step * x;
+                if (flag == 2)
+                	neighbor_ind = index + ng_step * y * x;
+
+
+                if (strcmp(type_name, PIDX_DType.UINT8) == 0 || strcmp(type_name, PIDX_DType.UINT8_GA) == 0 || strcmp(type_name, PIDX_DType.UINT8_RGB) == 0)
+				{
+                	c_data = buf[index];
+                	c_neigb = buf[neighbor_ind];
+                	// Calculate wavelet coefficients and replace in the buffer
+                	buf[index] = (c_data + c_neigb)/2;
+                	buf[neighbor_ind] = buf[index] - c_neigb;
+				}
+				if (strcmp(type_name, PIDX_DType.INT16) == 0 || strcmp(type_name, PIDX_DType.INT16_GA) == 0 || strcmp(type_name, PIDX_DType.INT16_RGB) == 0)
+				{
+					// Covert unsigned char to short
+                	memcpy(&s_data, &buf[index * sizeof(short)], sizeof(short));
+                	memcpy(&s_neigb, &buf[neighbor_ind * sizeof(float)], sizeof(short));
+                	// Calculate wavelet coefficients
+                	short avg = (s_data + s_neigb) / 2;
+                	short dif = avg - s_neigb;
+                	// Replace buffer
+                	memcpy(&buf[index * sizeof(short)], &avg, sizeof(short));
+                	memcpy(&buf[neighbor_ind * sizeof(short)], &dif, sizeof(short));
+				}
+				if (strcmp(type_name, PIDX_DType.INT32) == 0 || strcmp(type_name, PIDX_DType.INT32_GA) == 0 || strcmp(type_name, PIDX_DType.INT32_RGB) == 0)
+				{
+					// Covert unsigned char to int
+                	memcpy(&i_data, &buf[index * sizeof(int)], sizeof(int));
+                	memcpy(&i_neigb, &buf[neighbor_ind * sizeof(int)], sizeof(int));
+                	// Calculate wavelet coefficients
+                	int avg = (i_data + i_neigb) / 2;
+                	int dif = avg - i_neigb;
+                	// Replace buffer
+                	memcpy(&buf[index * sizeof(int)], &avg, sizeof(int));
+                	memcpy(&buf[neighbor_ind * sizeof(int)], &dif, sizeof(int));
+				}
+				else if (strcmp(type_name, PIDX_DType.FLOAT32) == 0 || strcmp(type_name, PIDX_DType.FLOAT32_GA) == 0 || strcmp(type_name, PIDX_DType.FLOAT32_RGB) == 0)
+				{
+					// Covert unsigned char to float
+                	memcpy(&f_data, &buf[index * sizeof(float)], sizeof(float));
+                	memcpy(&f_neigb, &buf[neighbor_ind * sizeof(float)], sizeof(float));
+                	// Calculate wavelet coefficients
+                	float avg = (f_data + f_neigb) / 2.0;
+                	float dif = avg - f_neigb;
+                	// Replace buffer
+                	memcpy(&buf[index * sizeof(float)], &avg, sizeof(float));
+                	memcpy(&buf[neighbor_ind * sizeof(float)], &dif, sizeof(float));
+				}
+				else if (strcmp(type_name, PIDX_DType.FLOAT64) == 0 || strcmp(type_name, PIDX_DType.FLOAT64_GA) == 0 || strcmp(type_name, PIDX_DType.FLOAT64_RGB) == 0)
+				{
+					// Covert unsigned char to double
+                	memcpy(&d_data, &buf[index * sizeof(double)], sizeof(double));
+                	memcpy(&d_neigb, &buf[neighbor_ind * sizeof(double)], sizeof(double));
+                	// Calculate wavelet coefficients
+                	double avg = (d_data + d_neigb) / 2.0;
+                	double dif = avg - d_neigb;
+                	// Replace buffer
+                	memcpy(&buf[index * sizeof(double)], &avg, sizeof(double));
+                	memcpy(&buf[neighbor_ind * sizeof(double)], &dif, sizeof(double));
+				}
+				else if (strcmp(type_name, PIDX_DType.INT64) == 0 || strcmp(type_name, PIDX_DType.INT64_GA) == 0 || strcmp(type_name, PIDX_DType.INT64_RGB) == 0)
+				{
+					// Covert unsigned char to int64_t
+                	memcpy(&i64_data, &buf[index * sizeof(int64_t)], sizeof(int64_t));
+                	memcpy(&i64_neigb, &buf[neighbor_ind * sizeof(int64_t)], sizeof(int64_t));
+                	// Calculate wavelet coefficients
+                	int64_t avg = (i64_data + i64_neigb) / 2.0;
+                	int64_t dif = avg - i64_neigb;
+                	// Replace buffer
+                	memcpy(&buf[index * sizeof(int64_t)], &avg, sizeof(int64_t));
+                	memcpy(&buf[neighbor_ind * sizeof(int64_t)], &dif, sizeof(int64_t));
+				}
+				else if (strcmp(type_name, PIDX_DType.UINT64) == 0 || strcmp(type_name, PIDX_DType.UINT64_GA) == 0 || strcmp(type_name, PIDX_DType.UINT64_RGB) == 0)
+				{
+					// Covert unsigned char to uint64_t
+                	memcpy(&u64i_data, &buf[index * sizeof(uint64_t)], sizeof(uint64_t));
+                	memcpy(&u64i_neigb, &buf[neighbor_ind * sizeof(uint64_t)], sizeof(uint64_t));
+                	// Calculate wavelet coefficients
+                	uint64_t avg = (u64i_data + u64i_neigb) / 2.0;
+                	uint64_t dif = avg - u64i_neigb;
+                	// Replace buffer
+                	memcpy(&buf[index * sizeof(uint64_t)], &avg, sizeof(uint64_t));
+                	memcpy(&buf[neighbor_ind * sizeof(uint64_t)], &dif, sizeof(uint64_t));
+				}
+            }
+        }
+    }
+}
+
+// Wavelet transform
+void PIDX_wavelet_transform(unsigned char* buffer, uint64_t x, uint64_t y, uint64_t z, int bits, char* type_name)
+{
+	// Calculate the max wavelet level based on the min dimensional value
+	uint64_t size[3] = {x, y, z};
+	int min = size[0];
+	for (int i = 1; i < 3; i++)
+	{
+		if (size[i] < min)
+			min = size[i];
+	}
+
+	int max_wavelet_level = log2(min);
+
+	// Get random wavelet level ( >= 1)
+	time_t t;
+	srand((unsigned) time(&t));
+	int wavelet_level = rand()%max_wavelet_level;
+	wavelet_level = (wavelet_level < 1) ? 1: wavelet_level;
+
+//	int wavelet_level = 1;
+
+	for (int level = 1; level <= wavelet_level; level++)
+	{
+		int step = pow(2, level);
+		int ng_step = step/2;
+
+        // Calculate x-dir
+        wavelet_helper(buffer, step, ng_step, 0, bits, x, y, z, type_name);
+        // Calculate y-dir
+        wavelet_helper(buffer, step, ng_step, 1, bits, x, y, z, type_name);
+        // Calculate z-dir
+        wavelet_helper(buffer, step, ng_step, 2, bits, x, y, z, type_name);
+	}
+}
+
+
 PIDX_return_code PIDX_brick_res_precision_rst_buf_aggregated_write(PIDX_brick_res_precision_rst_id rst_id)
 {
   int g = 0;
@@ -277,6 +447,9 @@ PIDX_return_code PIDX_brick_res_precision_rst_buf_aggregated_write(PIDX_brick_re
   strncpy(directory_path, rst_id->idx->filename, strlen(rst_id->idx->filename) - 4);
 
   PIDX_variable var0 = rst_id->idx->variable[rst_id->first_index];
+
+//  printf("rank %d: number_of_bricks: %d\n", rst_id->idx_c->simulation_rank, var0->brick_res_precision_io_restructured_super_patch_count);
+
   for (g = 0; g < var0->brick_res_precision_io_restructured_super_patch_count; ++g)
   {
     // loop through all groups
@@ -296,21 +469,67 @@ PIDX_return_code PIDX_brick_res_precision_rst_buf_aggregated_write(PIDX_brick_re
       PIDX_variable var_start = rst_id->idx->variable[v_start];
       PIDX_patch out_patch = var_start->brick_res_precision_io_restructured_super_patch[g]->restructured_patch;
 
+//      printf("rank %d: %dx%dx%d\n", rst_id->idx_c->simulation_rank, out_patch->size[0], out_patch->size[1], out_patch->size[2]);
+
+
       int bits = 0;
       PIDX_variable var = rst_id->idx->variable[v_start];
       bits = (var->bpv/8) * var->vps;
+
+      // If the patch size is less than the brick size (e.g., 32x24x32), this patch should be padding with 0 to be 32x32x32.
+      unsigned char* buf = var_start->brick_res_precision_io_restructured_super_patch[g]->restructured_patch->buffer;
+      uint64_t patch_x = rst_id->restructured_grid->patch_size[0];
+      uint64_t patch_y = rst_id->restructured_grid->patch_size[1];
+      uint64_t patch_z = rst_id->restructured_grid->patch_size[2];
+
+      uint64_t buffer_size =  out_patch->size[0] * out_patch->size[1] * out_patch->size[2];
+      uint64_t size = patch_x * patch_y * patch_z;
+
+      if (buffer_size < size)
+      {
+//    	  printf("%dx%dx%d\n", out_patch->size[0], out_patch->size[1], out_patch->size[2]);
+    	  unsigned char* res_buf = calloc(size * bits, sizeof(unsigned char));
+    	  int index1 = 0; int index2 = 0;
+    	  for (int i = 0; i < out_patch->size[2]; i++)
+    	  {
+    		  for (int j = 0; j < out_patch->size[1]; j++)
+    		  {
+    			  memcpy(&res_buf[index2], &buf[index1], out_patch->size[0] * bits);
+    			  index1 = i * out_patch->size[1] * out_patch->size[0] + j * out_patch->size[0];
+    			  index2 = i * patch_y * patch_x + j * patch_x;
+    		  }
+    	  }
+    	  // pass this restructured buffer to wavelet transform
+    	  PIDX_wavelet_transform(res_buf, patch_x, patch_y, patch_z, bits, var->type_name);
+    	  free(res_buf);
+      }
+      else
+      {
+    	  // pass original buffer to wavelet transform
+    	  PIDX_wavelet_transform(buf, patch_x, patch_y, patch_z, bits, var->type_name);
+      }
+
+//      float a = 0;
+//      if (var0->brick_res_precision_io_restructured_super_patch[g]->global_id == 0)
+//      {
+//    	  for (int i = 0; i < size; i++)
+//    	  {
+//    		  memcpy(&a, &buf[i*bits], bits);
+//    		  printf("%f\n", a);
+//    	  }
+//      }
 
       uint64_t data_offset = 0, v1 = 0;
       for (v1 = 0; v1 < v_start; v1++)
         data_offset = data_offset + (out_patch->size[0] * out_patch->size[1] * out_patch->size[2] * (rst_id->idx->variable[v1]->vps * (rst_id->idx->variable[v1]->bpv/8)));
 
-      uint64_t buffer_size =  out_patch->size[0] * out_patch->size[1] * out_patch->size[2] * bits;
-      uint64_t write_count = pwrite(fp, var_start->brick_res_precision_io_restructured_super_patch[g]->restructured_patch->buffer, buffer_size, data_offset);
-      if (write_count != buffer_size)
-      {
-        fprintf(stderr, "[%s] [%d] pwrite() failed.\n", __FILE__, __LINE__);
-        return PIDX_err_io;
-      }
+//      uint64_t buffer_size =  out_patch->size[0] * out_patch->size[1] * out_patch->size[2] * bits;
+//      uint64_t write_count = pwrite(fp, var_start->brick_res_precision_io_restructured_super_patch[g]->restructured_patch->buffer, buffer_size, data_offset);
+//      if (write_count != buffer_size)
+//      {
+//        fprintf(stderr, "[%s] [%d] pwrite() failed.\n", __FILE__, __LINE__);
+//        return PIDX_err_io;
+//      }
     }
     close(fp);
     free(file_name);
